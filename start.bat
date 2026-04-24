39d76a10dc4a7f7681933c36ef3875ab1d94bc25
mvale005 <mvale005@gmail.com>
2026-04-24 18:07:58 -0500

    cleaned up unused media.js and update start script

--- start.bat
+++ start.bat
@@ -3,12 +3,6 @@ cd C:\presentation-host
 echo Updating code...
 git pull
 
-echo Clearing local slides...
-if exist C:\presentation-host\public\slides (
-    del /Q C:\presentation-host\public\slides\*.PNG
-)
-
-echo Local slides cleared.
 
 set SERVER_URL=wss://remote.mvapphub.com
 set ROOM_CODE=ROOM1
